5b47a9cdb7bc360c2596d4f554236f9cf29aa68a
Olumide Daramola <olumidedaramola426@gmail.com>
2023-08-29 23:32:01 +0100

    input_validation

--- main.py
+++ main.py
@@ -15,3 +15,16 @@ print(
 wins = 0
 loses = 0
 ties = 0
+
+while True:  # Main game loop
+    while True:  # Keep asking until player enters R, P, S, or Q
+        print("{} wins, {} loses, {} ties}".format(wins, loses, ties))
+        print("Enter your move: (R)ock, (P)aper, (S)cissors or (Q)uit")
+        player_move = input("> ").upper()
+        if player_move == "Q":
+            print("Thanks for playing")
+            sys.exit()
+        if player_move == "R" or player_move == "P" or player_move == "S":
+            break
+        else:
+            print("Type one of R, P, S or Q")
