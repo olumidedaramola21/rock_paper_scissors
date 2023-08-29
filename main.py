81cab6fdc8b618529b331e1252814ee156eb5d15
Olumide Daramola <olumidedaramola426@gmail.com>
2023-08-30 00:15:56 +0100

    computer_move

--- main.py
+++ main.py
@@ -47,3 +47,14 @@ while True:  # Main game loop
     print("2...")
     time.sleep(0.25)
     print("3...")
+
+    # Display what the computer chose:
+    random_number = random.randint(1, 3)
+    if random_number == 1:
+        computer_move = "Rock"
+    elif random_number == 2:
+        computer_move = "Paper"
+    elif random_number == 3:
+        computer_move = "Scissors"
+    print(computer_move)
+    time.sleep(0.25)
