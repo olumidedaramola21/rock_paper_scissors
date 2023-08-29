c8916f77a14099d12e901dffa301b88a2567b419
Olumide Daramola <olumidedaramola426@gmail.com>
2023-08-29 23:21:06 +0100

    game rules.

---
+++ main.py
@@ -0,0 +1,17 @@
+"""Rock, Paper, Scissors"""
+"""The classic hand game of luck."""
+
+import random, time, sys
+
+print(
+    """Rock, Paper, Scissors game
+- Rock beats scissors.
+- Paper beats rocks.
+- Scissors beats paper.
+ """
+)
+
+# These variables keep track of wins, losses and ties
+wins = 0
+loses = 0
+tries = 0
